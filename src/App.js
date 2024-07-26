67bdd657a59c272656ef54d6d9659838caa451c9
Lenka.Weiss <lenka.weiss@roche.com>
2024-07-26 10:44:07 +0200

    Add initial houses data and fetch functionality
    Updated `App.js` to fetch house data from a new `houses.json` file. Introduced `useEffect` for fetching data and `useMemo` for selecting a random featured house.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import {useEffect, useState, useMemo} from "react";
 import './App.css';
 import Header from "./header";
 
 function App() {
+    const [allHouses, setAllHouses] = useState([]);
+    useEffect(() => {
+        const fetchHouses = async () => {
+            const rsp = await fetch('/houses.json');
+            const houses = await rsp.json();
+            setAllHouses(houses);
+        };
+        fetchHouses();
+    }, []);
+
+    const featuredHouse = useMemo (() => {
+        if (allHouses.length) {
+            const randomIndex = Math.floor(Math.random() * allHouses.length);
+            return allHouses[randomIndex];
+        }
+    }, [allHouses]);
+
   return (
     <div className="container">
       <Header
-          title='Some title'
           subtitle='Providing houses all over the Slovakia!'/>
     </div>
   );
