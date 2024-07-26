841ccae5d3c201e82de1e4ca77108967d1395ad2
Lenka.Weiss <lenka.weiss@roche.com>
2024-07-26 12:10:18 +0200

    Add React Router for client-side navigation
    Integrated 'react-router-dom' to manage client-side routing. Updated `App.js` to use `BrowserRouter` and set up basic route structure. Adjusted package dependencies to include `react-router-dom` and related packages.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useEffect, useState, useMemo} from "react";
 import './App.css';
 import Header from "./header";
+import {Router, Route, Routes, BrowserRouter} from "react-router-dom";
 
 function App() {
     const [allHouses, setAllHouses] = useState([]);
@@ -13,19 +14,24 @@ function App() {
         fetchHouses();
     }, []);
 
-    const featuredHouse = useMemo (() => {
+    const featuredHouse = useMemo(() => {
         if (allHouses.length) {
             const randomIndex = Math.floor(Math.random() * allHouses.length);
             return allHouses[randomIndex];
         }
     }, [allHouses]);
 
-  return (
-    <div className="container">
-      <Header
-          subtitle='Providing houses all over the Slovakia!'/>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Header subtitle='Providing houses all over the Slovakia!'/>
+
+                <Routes>
+                    <Route path="/"></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
